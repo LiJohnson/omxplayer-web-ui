d0b75740f28551e0e1e55b849b49f7f6a79bf7bd
LiJohnson <598420668@163.com>
2014-12-06 15:13:27 +0800

    服务端也好了

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-
 var app = require("express")();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-var omxplayer = require("./omxplayer");
+var omxplayer = require("omxctrl");
 
 var control = {
-  decreaseSpeed: '1',
-  increaseSpeed: '2',
-  previousAudioStream: 'j',
-  nextAudioStream: 'k',
-  previousChapter: 'i',
-  nextChapter: 'o',
-  previousSubtitleStream: 'n',
-  nextSubtitleStream: 'm',
-  toggleSubtitles: 's',
-  decreaseSubtitleDelay: 'd',
-  increaseSubtitleDelay: 'f',
-  pause: 'pause',
-  stop: 'stop',
-  decreaseVolume: 'volume-down',
-  increaseVolume: 'volume-up',
-  seekForward: "\x5b\x43",
-  seekBackward: "\x5b\x44",
-  seekFastForward: "\x5b\x41",
-  seekFastBackward: "\x5B\x42"
+//decreaseSpeed: '1',
+//increaseSpeed: '2',
+//previousAudioStream: 'j',
+//nextAudioStream: 'k',
+//previousChapter: 'i',
+//nextChapter: 'o',
+//previousSubtitleStream: 'n',
+//nextSubtitleStream: 'm',
+//toggleSubtitles: 's',
+//decreaseSubtitleDelay: 'd',
+//increaseSubtitleDelay: 'f',
+	pause: 'pause',
+	stop: 'stop',
+	decreaseVolume: 'volume-down',
+	increaseVolume: 'volume-up'
+//seekForward: "\x5b\x43",
+//seekBackward: "\x5b\x44",
+//seekFastForward: "\x5b\x41",
+//seekFastBackward: "\x5B\x42"
 };
 
 var file = "http://mr3.douban.com/201412040332/8bb51559e63654163053d64a28d79519/view/song/small/p753643.mp4";
-//omxctrl.play(file);
+
+var handleControl = function(action){
+	if( action.name == 'play' ){
+		omxplayer.play(action.file,[]);
+	}else{
+		omxplayer[action.name]();
+	}
+};
+
 //omxctrl.play(file);
 
 app.get("/",function(req,res){	
@@ -35,27 +42,33 @@ app.get("/",function(req,res){
 	res.sendFile(file);
 });
 
-app.get("/static/*",function(req,res){	
+app.get("/static/*",function(req,res){
 	var file = __dirname + "/lib" + req._parsedUrl.pathname.replace(/^\/?static/,'');
 	console.log(file);
 	res.sendFile(file);
 });
 
+app.get("/action/:name",function(req,res){
+	handleControl({name:req.params.name , file:req.query.file});
+	res.send("ko");
+});
+
 io.on("connection",function(socket){
 	socket.emit("control",control);
 	socket.on("control",function(action){
-		
-		if( action.name == 'play' ){
-			omxplayer.play(action.file);
-		}else{
-			omxplayer[action.name]();
-		}
+		handleControl(action);
 	});
-})
-
-server.listen(9090,function(){
-	console.log("ha",9090);
 });
 
+omxplayer.on("playing",function(file){
+	"stdout,stderr".split(',', function(std){
+		omxplayer.player[std].on("data",function(data){
+			console.log( std + " data " + data);
+		});
+	});
+	console.log("playing " + file);
+});
 
-
+server.listen(9191,function(){
+	console.log("ha",9191);
+});
